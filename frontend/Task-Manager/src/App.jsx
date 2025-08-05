0a2024d063fcbc35f89c17a168993ae96c9d35af
Nandini0308 <70321229+Nandini0308@users.noreply.github.com>
2025-08-05 10:56:13 +0000

    Setup - 3

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Dashboard from './pages/Admin/Dashboard';
+import CreateTask from './pages/Admin/CreateTask';
+import ManageTasks from './pages/Admin/ManageTask';
+import ManageUsers from './pages/Admin/ManageUsers';
+
+import UserDashboard from './pages/User/UserDashboard';
+import MyTasks from './pages/User/MyTasks';
+import ViewTaskDetails from './pages/User/ViewTaskDetails';
+
+import PrivateRoute from './routes/PrivateRoute';
 
 const App = () => {
   return (
@@ -15,6 +27,13 @@ const App = () => {
             <Route path='/admin/tasks' element={<ManageTasks />}/>
             <Route path='/admin/create-task' element={<CreateTask />}/>
             <Route path='/admin/users' element={<ManageUsers />}/>
+
+          {/*User Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}/>
+            <Route path='/user/dashboard' element={<UserDashboard />}/>
+            <Route path='/user/tasks' element={<MyTasks />}/>
+            <Route path='/admin/task-details/:id' element={<ViewTaskDetails />}/>
+
         </Routes>
       </Router>
     </div>
